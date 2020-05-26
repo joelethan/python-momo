a610587aedeec816af2bd7950eb013033330cd22
joelethan <joelkatsme@gmail.com>
2020-05-26 08:15:57 +0300

    Check if userexists and create an API key for the user

--- index.py
+++ index.py
@@ -15,3 +15,25 @@ def createUser():
         print(r.status_code, r.text)
     except Exception as e:
         pass
+
+def getAPIKey():
+    headers = {
+        'Ocp-Apim-Subscription-Key': env.env_var['Primary-Key']
+    }
+    url = 'https://sandbox.momodeveloper.mtn.com/v1_0/apiuser/{}/apikey'.format(env.env_var['X-Reference-Id-User'])
+    try:
+        r = requests.post(url, headers=headers)
+        print(r.status_code, r.text)
+    except Exception as e:
+        pass
+
+def getUser():
+    headers = {
+        'Ocp-Apim-Subscription-Key': env.env_var['Primary-Key'],
+    }
+    url = 'https://sandbox.momodeveloper.mtn.com/v1_0/apiuser/%s'%env.env_var['X-Reference-Id-User']
+    try:
+        r = requests.get(url, headers=headers)
+        print(r.status_code, r.text)
+    except Exception as e:
+        pass
